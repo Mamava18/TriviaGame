8aeaf6fc7ab560fa9a6e9b0ce9af91dbd9795e3d
Mamava18 <44853413+Mamava18@users.noreply.github.com>
2019-01-27 02:11:44 -0500

    Add more updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,8 +26,7 @@ var incorrect = 0;
 var timer = 30;
 var timeInterval;
 
-
-$('#submit').on('click', function () {
+$("#submit").on("click", function () {
     for (var i = 0; i < trivia.length; i++) {
         if (results[i] == trivia[i].correctAnswer) {
             correct++
@@ -50,26 +49,41 @@ $("#startBtn").on("click", function () {
 function displayQuestions() {
     for (var i = 0; i < trivia.length; i++) {
         var currentQuestion = trivia[i];
-        $('#questionsDisplay').append(`<div>${currentQuestion.triviaQuestion}</div>`);
+        $("#questionsDisplay").append(`<div>${currentQuestion.triviaQuestion}</div>`);
         for (var j = 0; j < currentQuestion.options.length; j++) {
-            $('#questionsDisplay').append(`<input question-number="${i}" type="radio" data-value="${currentQuestion.options[j]}" class="option">${currentQuestion.options[j]}<br>`)
+            var currentOption = trivia[i].options[j];
+            //$("#questionsDisplay").append(`<input question-number="${i}" name="triviaOption" type="radio" data-value="${currentOption}" class="option">${currentOption}<br>`);
+            $("#questionsDisplay").append(`<input question-number="${i}" type="radio" data-value="${currentOption}" class="option">${currentOption}<br>`);
         }
+        // $(this.currentOption).each(function checkRadio() {
+        //     var radioOption = document.getElementsByName("triviaOpition");
+        //     for (var i=0; i < radioOption.length; i++) {
+        //         if (radioOption[i].checked) {
+        //             break;
+        //         }
+        //     }
+        //     checkRadio();
+        //     alert("This function works!");
+        // });
+        
     }
     
     $("#submit").css({
         "visibility": "visible"
     });
 
-    $('.option').on('click', function () {
-        var value = $(this).attr('data-value');
-        var number = $(this).attr('question-number');
+    $(".option").on("click", function () {
+        var value = $(this).attr("data-value");
+        var number = $(this).attr("question-number");
         results[number] = value;    
     });
+
+    
 }
 
 function displayResults() {
     $("#results").append(`<p id="correctAns">The number of correct answers is ${correct}</p>`);
-    $("#results").append(`<p id="incorrectAns">The number of correct answers is ${incorrect}</p>`);
+    $("#results").append(`<p id="incorrectAns">The number of incorrect answers is ${incorrect}</p>`);
     $("#resetBtn").css({
         "visibility": "visible"
     });
@@ -80,10 +94,13 @@ function startGame() {
     timeInterval = setInterval(function () {
         if (timer > 0) {
             timer--
-            $('#timer').html('<div>' + timer + '</div>');
+            $("#timer").html(`<div>Time left: ${timer} seconds</div>`);
+            if (timer === 1) {
+                $("#timer").html(`<div>Time left: ${timer} second</div>`); 
+            }
         } else {
             clearInterval(timeInterval)
-            $('#timer').html("<div>Time is up!</div>");
+            $("#timer").html("<div>Time is up!</div>");
             $("#submit").css({
                 "visibility": "hidden"
             });
